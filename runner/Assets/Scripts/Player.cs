d893eea395ccec7cdbfa166dfa3c485c5b757ead
alexandre caila <alexandrecaila26@gmail.com>
2020-08-18 02:19:45 +0200

    fix

--- runner/Assets/Scripts/Player.cs
+++ runner/Assets/Scripts/Player.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.IO.Ports;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
--- runner/Assets/Scripts/PlayerMovement.cs
+++ runner/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,4 @@
-﻿using Packages.Rider.Editor.PostProcessors;
-using PathCreation;
+﻿using PathCreation;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
